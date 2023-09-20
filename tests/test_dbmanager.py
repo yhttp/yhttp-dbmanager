fca07399ff452438b2dbf49fffd131be8ffa2c3a
pylover <vahid.mardani@gmail.com>
2023-09-20 19:10:46 +0330

    Implementing dbmanager

--- tests/test_dbmanager.py
+++ tests/test_dbmanager.py
@@ -4,16 +4,16 @@
 DBNAME = 'yhttp-postgesql-testing'
 
 
-# def test_dbmanager():
-#     # TODO: create
-#     # TODO: execute
-#     # TODO: drop
-#     # TODO: dropifexist
-#     # TODO: exists
-#     # TODO: DB cleanup fixture to remover db
-#
-#     # Instance with postgres identity framework.
-#     dbm = DatabaseManager()
-#
-#     assert dbm.exists('postgres')
-#     assert not dbm.exists(DBNAME)
+def test_dbmanager():
+    # TODO: create
+    # TODO: execute
+    # TODO: drop
+    # TODO: dropifexist
+    # TODO: exists
+    # TODO: DB cleanup fixture to remover db
+
+    # Instance with postgres identity framework.
+    dbm = DatabaseManager()
+
+    assert dbm.exists('postgres')
+    assert not dbm.exists(DBNAME)
