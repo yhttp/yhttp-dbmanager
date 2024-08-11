fc175ea873847b2ec06be97672a16a087c6f7110
pylover <vahid.mardani@gmail.com>
2024-08-12 02:37:55 +0330

    More conveniece tests for dburi

--- tests/test_dburi.py
+++ tests/test_dburi.py
@@ -18,3 +18,33 @@ def test_dburi():
     uristr = 'baduri'
     with pytest.raises(ValueError):
         DatabaseURI.loads(uristr)
+
+    # Without password
+    uristr = 'foo://bar:@qux/quux.thud'
+    uri = DatabaseURI.loads(uristr)
+    assert uri.provider == 'foo'
+    assert uri.user == 'bar'
+    assert uri.password == ''
+    assert uri.host == 'qux'
+    assert uri.dbname == 'quux.thud'
+    assert uri.dumps() == uristr
+
+    # Without username and password
+    uristr = 'foo://:@qux/quux.thud'
+    uri = DatabaseURI.loads(uristr)
+    assert uri.provider == 'foo'
+    assert uri.user == ''
+    assert uri.password == ''
+    assert uri.host == 'qux'
+    assert uri.dbname == 'quux.thud'
+    assert uri.dumps() == uristr
+
+    # Without username, password and host
+    uristr = 'foo://:@/quux.thud'
+    uri = DatabaseURI.loads(uristr)
+    assert uri.provider == 'foo'
+    assert uri.user == ''
+    assert uri.password == ''
+    assert uri.host == ''
+    assert uri.dbname == 'quux.thud'
+    assert uri.dumps() == uristr
