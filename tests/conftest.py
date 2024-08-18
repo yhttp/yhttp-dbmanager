4666735a64ce0a312654281388a12141739c8c06
pylover <vahid.mardani@gmail.com>
2024-08-19 03:05:24 +0330

    Migrate to yhttp-dev >= 3.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,15 @@
 import os
 
 import pytest
+from yhttp.dev.fixtures import cicd
 from yhttp.ext.dbmanager import PostgresqlManager
-
-
-CICD = 'CI' in os.environ \
-    and os.environ['CI'] \
-    and 'GITHUB_RUN_ID' in os.environ
-
-
-@pytest.fixture
-def cicd():
-    return CICD
 
 
 @pytest.fixture
 def dbmanager(cicd):
-    host = CICD and 'localhost' or None
-    user = CICD and 'postgres' or None
-    pass_ = CICD and 'postgres' or None
+    host = cicd and 'localhost' or None
+    user = cicd and 'postgres' or None
+    pass_ = cicd and 'postgres' or None
     return PostgresqlManager(
         host=os.environ.get('YHTTP_DB_DEFAULT_HOST', host),
         user=os.environ.get('YHTTP_DB_DEFAULT_ADMINUSER', user),
